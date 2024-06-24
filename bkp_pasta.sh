9de4cf7f119ffefbc70fb0dbfeab7754c721f28c
gsoares12 <126172031+gsoares12@users.noreply.github.com>
2024-06-24 11:43:37 -0300

    Create bkp_pasta.sh

---
+++ bkp_pasta.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+origem="/home/suporte/devops/scripts"
+destino="/home/suporte/devops/scripts"
+data=$(date +"%Y%m%d")
+tar -czf $destino/backup_$data.tar.gz $origem
+suporte@suporte:~/devops/scripts$
