37dc09d12e81d8f74c1aabe8188de3ee1572b3bb
gsoares12 <126172031+gsoares12@users.noreply.github.com>
2024-06-24 11:54:47 -0300

    Update compactador.sh

--- compactador.sh
+++ compactador.sh
@@ -1,14 +1,5 @@
 #! /bin/bash
 
-diretorio_backup="/home/suporte/devops"
-nome_arquivo="backup_$(date +%Y%m%d_%H%M%S).tar.gz"
-tar -czf "$nome_arquivo" "diretorio_backup"
-echo "Backup concluído com sucesso em $nome_arquivo"
-
-suporte@suporte:~$ ^C
-suporte@suporte:~$ cat compactador.sh
-#! /bin/bash
-
 if [ "$#" -lt 2 ]; then
     echo "O programa, $0 requer nome do arquivo e arquivos a serem compactados"
     exit 1
