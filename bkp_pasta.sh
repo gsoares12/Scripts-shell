b8bb767c7d974b454e49817c6a88076f179254f6
gsoares12 <126172031+gsoares12@users.noreply.github.com>
2024-06-24 11:55:51 -0300

    Update bkp_pasta.sh

--- bkp_pasta.sh
+++ bkp_pasta.sh
@@ -4,4 +4,3 @@ origem="/home/suporte/devops/scripts"
 destino="/home/suporte/devops/scripts"
 data=$(date +"%Y%m%d")
 tar -czf $destino/backup_$data.tar.gz $origem
-suporte@suporte:~/devops/scripts$
